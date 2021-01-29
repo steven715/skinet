956ea55b6d7d7d4be4daddc77715ca3b93693b2b
Steven <yugioh821113@gmail.com>
2021-01-29 23:18:59 +0800

    End of section 19

--- client/src/app/checkout/checkout-routing.module.ts
+++ client/src/app/checkout/checkout-routing.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { pathToFileURL } from 'url';
 import { CheckoutComponent } from './checkout.component';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 
 const routes: Routes = [
-  { path: '', component: CheckoutComponent }
+  { path: '', component: CheckoutComponent },
+  { path: 'success', component: CheckoutSuccessComponent }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forChild(routes)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class CheckoutRoutingModule {}
